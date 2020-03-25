b81debfab2e50eccbbe47476ffacbe00455ca998
Bilal Rafiq <bilal-rafiq@outlook.com>
2020-03-25 17:55:31 +0500

    pages added

--- Controllers/AppointmentsController.cs
+++ Controllers/AppointmentsController.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace meeting.Controllers
 {
-    [Authorize]
+    //[Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class AppointmentsController : ControllerBase
